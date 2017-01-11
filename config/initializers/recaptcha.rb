20959c567560664b254ed19a25c5212782ed08d8
Robert Verrill <Robert_Verrill@baylor.edu>
2017-01-10 20:07:30 -0600

    captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
 
-  config.site_key  = '6LctYhEUAAAAABREHkwI0351SIs4VDVg5y-oT2xV'
+  config.site_key  = '6LcSYhEUAAAAAOrwZzo5IuoxVkNEI53v3v2yzEoy'
   config.secret_key = Rails.application.secrets.recaptcha_secret_key
 end
